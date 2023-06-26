4c74398116487e3f0a6e50d7eba019d879c750f2
Daniel Scharf <id.scharfd@gmail.com>
2023-06-26 11:49:19 +0900

    fixed some of the wording in my about me

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const info = "Daniel Scharf is a former English teacher, educator, and tutoring center administrator who has changed his career to software and web development. Daniel has about eight years of experience teaching English abroad, six years in tutoring center administration, and five years working with curriculum design. Outside of academia Daniel also worked as a cook for about 10 years. Daniel's personal interests include coding, computer games, Dungeons and Dragons, cooking, traveling, bike riding, acting, and napping with his cat.";
 
 const paragraph = document.querySelector('.paragraph');
-const original = "Daniel Scharf is a former English teacher, educator, and tutoring center administrator who is currently changing his career to software and web development.";
+const original = "Daniel Scharf is a former English teacher, educator, and tutoring center administrator who has changed his career to software and web development.";
 
 paragraph.onmouseover = function () {
   paragraph.innerText = info;
