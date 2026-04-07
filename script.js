82c9282822cd6894dccbbe17ae27fd86144198d1
Daniel Scharf <108774211+DanielBScharf@users.noreply.github.com>
2026-04-07 15:16:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const info = "Daniel Scharf is a former English teacher, educator, and tutoring center administrator who is currently changing his career to software and web development. Daniel has about eight years of experience teaching English abroad, six years in tutoring center administration, and five years working with curriculum design. Outside of academia Daniel also worked as a cook for about 10 years. Daniel's personal interests include computer games, Dungeons and Dragons, cooking, traveling, bike riding, and napping with his cat.";
+const info = "Daniel Scharf is a former English teacher, educator, and tutoring center administrator who is currently changing his career to software and web development. Daniel has about ten years of experience teaching English abroad, six years in tutoring center administration, and five years working with curriculum design. Outside of academia Daniel also worked as a cook for about 10 years. Daniel's personal interests include computer games, Dungeons and Dragons, cooking, traveling, bike riding, and napping with his cat.";
 
 const paragraph = document.querySelector('.paragraph');
-const original = "Daniel Scharf is a former English teacher, educator, and tutoring center administrator who is currently changing his career to software and web development.";
+const original = "Daniel Scharf is a former English teacher, educator, and tutoring center administrator, and web development dabbler.";
 
 paragraph.onmouseover = function () {
   paragraph.innerText = info;
